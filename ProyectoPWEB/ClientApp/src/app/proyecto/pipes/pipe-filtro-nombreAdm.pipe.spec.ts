f62de08b4f227e59ae0a427f1e7ebbce2e5917f0
Jefferson1506 <jeffersondpalacio@unicesar.edu.co>
2021-11-28 23:56:55 -0500

    el pipe

---
+++ ProyectoPWEB/ClientApp/src/app/proyecto/pipes/pipe-filtro-nombreAdm.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { PipeFiltroNombreAdmPipe } from './pipe-filtro-nombreAdm.pipe';
+
+describe('Pipe: PipeFiltroNombreAdme', () => {
+  it('create an instance', () => {
+    let pipe = new PipeFiltroNombreAdmPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
