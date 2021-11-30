68134cd411b69a126168f7f1a39ae1cfc1bf52ee
Sirmauricio10000 <mauricio1907_5@outlook.com>
2021-11-30 00:49:36 -0500

    No despliega bien

--- ProyectoPWEB/ClientApp/src/app/proyecto/Funcionario/calificar-proyectos/calificar-proyectos.component.ts
+++ ProyectoPWEB/ClientApp/src/app/proyecto/Funcionario/calificar-proyectos/calificar-proyectos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AlertModalComponent } from 'src/app/@base/alert-modal/alert-modal.component';
+import { ProyectoService } from 'src/app/services/proyecto.service';
+import { Proyecto } from '../../models/proyecto';
 
 @Component({
   selector: 'app-calificar-proyectos',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalificarProyectosComponent implements OnInit {
   filtro: string;
-  constructor() { }
+  constructor(private proyectoService: ProyectoService, private modalService: NgbModal) { }
+
+  proyectos: Proyecto[];
+  proyecto: Proyecto;
+  codigo: number;
+  calificacion: string;
+  comentarios: string;
+
 
   ngOnInit() {
+    this.proyectoService.get().subscribe(result => {this.proyectos = result;});
+  }
+
+  calificar(){
+    this.proyectoService.getByCode(this.codigo).subscribe(result => {
+      this.proyecto = result;
+      this.proyecto.estadoProyecto = this.calificacion;
+      this.proyecto.comentariosProyecto = this.comentarios;
+
+      this.proyectoService.put(this.proyecto).subscribe(p=>{
+        if(p!=null){
+          const messageBox = this.modalService.open(AlertModalComponent)
+          messageBox.componentInstance.title = "Resultado Operación";
+          messageBox.componentInstance.message = 'Proyecto calificado correctamente';
+        }
+      })
+    });
   }
 
 }
